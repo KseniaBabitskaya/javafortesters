2f3d45269bb5530969ef6bf947b509614ff38269
KseniaBabitskaya <fksenium@gmail.com>
2016-02-23 21:21:17 +0300

    Добавлено задание №2: функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,28 +1,19 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		hello("world");
 		hello("user");
 		hello("Ksenia");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " =	" + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " =	" + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольнка со сторонами " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольнка со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody){
-		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area(double len){
-		return len * len;
-	}
-
-	public static double area(double a, double b){
-		return a * b;
+				System.out.println("Hello, " + somebody + "!");
 	}
 }                               
