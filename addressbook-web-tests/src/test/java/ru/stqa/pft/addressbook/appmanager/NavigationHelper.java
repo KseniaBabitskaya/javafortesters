80052bfbc9420059cee804f40ac2c33c823602bc
fedotk <fksenium@gmail.com>
2016-02-29 17:58:46 +0300

    Добавлены специализированные классы-помошники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by fedotk on 2/29/2016.
+ */
+public class NavigationHelper {
+  public FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+      wd.findElement(By.linkText("groups")).click();
+  }
+}
