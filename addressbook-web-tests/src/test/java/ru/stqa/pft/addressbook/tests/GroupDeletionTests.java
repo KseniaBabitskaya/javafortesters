0d898a41f42531c6429fe8f4aa5cc70809cc5a67
fedotk <fksenium@gmail.com>
2016-03-23 18:43:20 +0300

    Добавлено использование уникальных идентификаторов объектов - groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -5,34 +5,29 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
-    @BeforeMethod
-    public void ensurePreconditions() {
-        app.goTo().groupPage();
-        if (!(app.group().list().size() == 0)) {
-            app.group().create(new GroupData().withName("test1"));
-        }
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().groupPage();
+    if (!(app.group().all().size() == 0)) {
+      app.group().create(new GroupData().withName("test1"));
     }
+  }
 
-    @Test
-    public void testGroupDeletion() {
-        app.goTo().groupPage();
-        List<GroupData> before = app.group().list();
-        int index = (before.size() - 1);
-        app.group().delete(index);
-        List<GroupData> after = app.group().list();
-        Assert.assertEquals(after.size(), before.size() - 1);
-        before.remove(index);
+  @Test
+  public void testGroupDeletion() {
+    app.goTo().groupPage();
+    Set<GroupData> before = app.group().all();
+    GroupData deletedGroup = before.iterator().next();
+    app.group().delete(deletedGroup);
+    Set<GroupData> after = app.group().all();
 
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
+    Assert.assertEquals(after.size(), before.size() - 1);
+    before.remove(deletedGroup);
+    Assert.assertEquals(before, after);
 
-        Assert.assertEquals(before, after);
-
-    }
+  }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,42 +5,33 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 /**
  * Created by fedotk on 3/2/2016.
  */
 public class GroupModificationTests extends TestBase {
-    @BeforeMethod
-    public void ensurePreconditions() {
-        app.goTo().groupPage();
-        if (!(app.group().list().size() == 0)) {
-            app.group().create(new GroupData().withName("test1"));
-        }
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().groupPage();
+    if (!(app.group().all().size() == 0)) {
+      app.group().create(new GroupData().withName("test1"));
     }
+  }
 
-    @Test
-    public void testGroupModification() {
-        app.goTo().groupPage();
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
-        GroupData group = new GroupData().withID(before.get(index).getId()).withName("test11").withHeader("test2").withFooter("test3");
-
-        app.group().modify(index, group);
-
-        app.goTo().groupPage();
-
-        List<GroupData> after = app.group().list();
-        Assert.assertEquals(after.size(), before.size());
-        before.remove(before.size() - 1);
-        before.add(group);
-
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
-//    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); //сравнение множеств (неупорядоченный список)
-        Assert.assertEquals(before, after);
-    }
+  @Test
+  public void testGroupModification() {
+    app.goTo().groupPage();
+    Set<GroupData> before = app.group().all();
+    GroupData modifiedGroup = before.iterator().next();
+    GroupData group = new GroupData().withID(modifiedGroup.getId()).withName("test11").withHeader("test2").withFooter("test3");
+    app.group().modify(group);
+    app.goTo().groupPage();
+    Set<GroupData> after = app.group().all();
+    Assert.assertEquals(after.size(), before.size());
+    before.remove(modifiedGroup);
+    before.add(group);
+    Assert.assertEquals(before, after);
+  }
 
 }
