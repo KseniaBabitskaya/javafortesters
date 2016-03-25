ef7f682c7a4565583d9d2a807c4603463d0f806e
KseniaBabitskaya <fksenium@gmail.com>
2016-03-25 23:23:08 +0300

    Изменены локаторы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -15,109 +15,104 @@ import java.util.List;
  */
 public class ContactHelper extends HelperBase {
 
-  public ContactHelper(WebDriver wd) {
-    super(wd);
-  }
+    public ContactHelper(WebDriver wd) {
+        super(wd);
+    }
 
-  public int count() {
-    return wd.findElements(By.name("selected[]")).size();
-  }
-  public void fillContactForm(ContactData contactData, boolean creation) {
-    type(By.name("firstname"), contactData.getFirstname());
-    type(By.name("lastname"), contactData.getLastname());
+    public int count() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public void fillContactForm(ContactData contactData, boolean creation) {
+        type(By.name("firstname"), contactData.getFirstname());
+        type(By.name("lastname"), contactData.getLastname());
 //        type(By.name("group"), contactData.getGroup());
-    if (creation) {
-      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
-    } else {
-      Assert.assertFalse(isElementPresent(By.name("new_group")));
+        if (creation) {
+            new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+        } else {
+            Assert.assertFalse(isElementPresent(By.name("new_group")));
+        }
     }
 
-//        click(By.xpath("//div[@id='content']/form/input[21]"));
-  }
-
-  public void submitContactCreation() {
-    click(By.name("submit"));
-  }
-
-  public void initContactCreation() {
-    click(By.linkText("add new"));
-  }
-
-  public void selectContact(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
-  }
-
-  public void selectContactById(int id) {
-    wd.findElement(By.cssSelector("input[id='" + id + "']")).click();
-  }
-
-  private void returnToHomePage() {
-      click(By.linkText("home"));
-//    click(By.xpath("//div[@class='msgbox']//a[.='home page']"));
-  }
-
-  public void submitContactDeletion() {
-    click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
-    wd.switchTo().alert().accept();
-
-  }
-
-  //  public void initContatModification(int number) {
-
-  public void initContatModification() {
-    String locator = "/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]";
-    click(By.xpath(locator));
-  }
-
-  public void submitModification() {
-    click(By.name("update"));
-  }
-
-  public boolean isThereAContact() {
-    return isElementPresent(By.name("selected[]"));
-  }
-
-  public void create(ContactData contact) {
-    initContactCreation();
-    fillContactForm(contact, true);
-    submitContactCreation();
-    contactCache = null;
-    returnToHomePage();
-  }
-
-
-  public void modify(ContactData contact) {
-    initContatModification();
-    fillContactForm(contact, false);
-    submitModification();
-    contactCache = null;
-    returnToHomePage();
-  }
-
-  public void delete(ContactData contact) {
-    selectContactById(contact.getId());
-    submitContactDeletion();
-    contactCache = null;
-    returnToHomePage();
-  }
-
-  private Contacts contactCache = null;
-
-  public Contacts all() {
-    if (contactCache != null){
-      return new Contacts(contactCache);
+    public void submitContactCreation() {
+        click(By.name("submit"));
     }
-    contactCache = new Contacts();
-    List<WebElement> rows = wd.findElements(By.tagName("tr"));
-    for (int i = 1; i < rows.size(); i++) {
-      String lastname = rows.get(i).findElements(By.tagName("td")).get(1).getText();
-      String firstname = rows.get(i).findElements(By.tagName("td")).get(2).getText();
-      int id = Integer.parseInt(rows.get(i).findElement(By.tagName("input")).getAttribute("value"));
-      ContactData contact = new ContactData().withId(id).withFirstname(firstname).withLastname(lastname);
-      contactCache.add(contact);
+
+    public void initContactCreation() {
+        click(By.linkText("add new"));
     }
-    return new Contacts(contactCache);
-  }
+
+    public void selectContact(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
+    }
+
+    public void selectContactById(int id) {
+        wd.findElement(By.cssSelector(String.format("input[id='%s'", id))).click();
+    }
+
+    private void returnToHomePage() {
+        click(By.linkText("home"));
+    }
+
+    public void submitContactDeletion() {
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+        wd.switchTo().alert().accept();
+
+    }
+
+    public void initContatModification(int id) {
+        wd.findElement((By.cssSelector(String.format("a[href='edit.php?id=%s']", id)))).click();
+    }
+
+    public void submitModification() {
+        click(By.name("update"));
+    }
+
+    public boolean isThereAContact() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    public void create(ContactData contact) {
+        initContactCreation();
+        fillContactForm(contact, true);
+        submitContactCreation();
+        contactCache = null;
+        returnToHomePage();
+    }
+
+
+    public void modify(ContactData contact) {
+        initContatModification(contact.getId());
+        fillContactForm(contact, false);
+        submitModification();
+        contactCache = null;
+        returnToHomePage();
+    }
+
+    public void delete(ContactData contact) {
+        selectContactById(contact.getId());
+        submitContactDeletion();
+        contactCache = null;
+        returnToHomePage();
+    }
+
+    private Contacts contactCache = null;
+
+    public Contacts all() {
+        if (contactCache != null) {
+            return new Contacts(contactCache);
+        }
+        contactCache = new Contacts();
+        List<WebElement> rows = wd.findElements(By.tagName("tr"));
+        for (int i = 1; i < rows.size(); i++) {
+            String lastname = rows.get(i).findElements(By.tagName("td")).get(1).getText();
+            String firstname = rows.get(i).findElements(By.tagName("td")).get(2).getText();
+            int id = Integer.parseInt(rows.get(i).findElement(By.tagName("input")).getAttribute("value"));
+            ContactData contact = new ContactData().withId(id).withFirstname(firstname).withLastname(lastname);
+            contactCache.add(contact);
+        }
+        return new Contacts(contactCache);
+    }
 
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by fedotk on 2/29/2016.
@@ -16,105 +14,105 @@ import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
-  public GroupHelper(WebDriver wd) {
-    super(wd);
-  }
-
-  public void returnToGroupPage() {
-    click(By.linkText("group page"));
-  }
-
-  public void submitGroupCreation() {
-    click(By.name("submit"));
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    type(By.name("group_name"), groupData.getName());
-    type(By.name("group_header"), groupData.getHeader());
-    type(By.name("group_footer"), groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    click(By.name("new"));
-  }
-
-  public void deleteSelectedGroups() {
-    click(By.name("delete"));
-  }
-
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
-  }
-
-  public void selectGroupById(int id) {
-    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
-  }
-
-  public void initGroupModification() {
-    click(By.name("edit"));
-  }
-
-  public void submitGroupModification() {
-    click(By.name("update"));
-  }
-
-  public boolean isThereAGroup() {
-    return isElementPresent(By.name("selected[]"));
-  }
-
-  public int count() {
-    return wd.findElements(By.name("selected[]")).size();
-  }
-
-  private void gotoGroupPage() {
-    if (isElementPresent(By.id("maintable"))) {
-      return;
-    }
-    click(By.linkText("home"));
-  }
-
-  public void create(GroupData group) {
-    initGroupCreation();
-    fillGroupForm(group);
-    submitGroupCreation();
-    groupCache = null;
-    returnToGroupPage();
-
-  }
-
-  public void delete(GroupData group) {
-    selectGroupById(group.getId());
-    deleteSelectedGroups();
-    groupCache = null;
-    returnToGroupPage();
-  }
-
-  public void modify(GroupData group) {
-    selectGroupById(group.getId());
-    initGroupModification();
-    fillGroupForm(group);
-    submitGroupModification();
-    groupCache = null;
-    returnToGroupPage();
-  }
-
-  private Groups groupCache = null;
-
-  public Groups all() {
-    if (groupCache != null){
-      return new Groups(groupCache);
+    public GroupHelper(WebDriver wd) {
+        super(wd);
     }
 
-    groupCache = new Groups();
-    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
-    for (WebElement element : elements) {
-      String name = element.getText();
-      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      GroupData group = new GroupData().withID(id).withName(name);
-      groupCache.add(group);
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
     }
-    return new Groups (groupCache);
-  }
+
+    public void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
+
+    public void selectGroup(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
+    }
+
+    public void selectGroupById(int id) {
+        wd.findElement(By.cssSelector(String.format("input[value='%s']", id))).click();
+    }
+
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    public int count() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    private void gotoGroupPage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("home"));
+    }
+
+    public void create(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        groupCache = null;
+        returnToGroupPage();
+
+    }
+
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
+        deleteSelectedGroups();
+        groupCache = null;
+        returnToGroupPage();
+    }
+
+    public void modify(GroupData group) {
+        selectGroupById(group.getId());
+        initGroupModification();
+        fillGroupForm(group);
+        submitGroupModification();
+        groupCache = null;
+        returnToGroupPage();
+    }
+
+    private Groups groupCache = null;
+
+    public Groups all() {
+        if (groupCache != null) {
+            return new Groups(groupCache);
+        }
+
+        groupCache = new Groups();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            GroupData group = new GroupData().withID(id).withName(name);
+            groupCache.add(group);
+        }
+        return new Groups(groupCache);
+    }
 
 
 }
