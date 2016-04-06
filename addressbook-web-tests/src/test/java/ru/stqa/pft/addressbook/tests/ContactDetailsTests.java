3fa42366a80bd4ec5d5e488b3f48c030e1d3d85e
fedotk <fksenium@gmail.com>
2016-04-06 16:59:31 +0300

    Исправила сравнение деталей

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDetailsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDetailsTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -7,22 +9,33 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Ксюшенька on 26.03.2016.
  */
-public class ContactDetailsTests extends TestBase{
+public class ContactDetailsTests extends TestBase {
 
-    @Test(enabled = false)
-    public void testContactDetails(){
-        app.goTo().homePage();
-        ContactData contact = app.contact().all().iterator().next();
-        ContactData contactInfoFromDetails = app.contact().infoFromDetailsForm(contact);
-        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+  @Test(enabled = true)
+  public void testContactDetails() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromDetails = app.contact().infoFromDetailsForm(contact);
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-        assertThat(contactInfoFromDetails, equalTo(contactInfoFromEditForm));
-    }
+    MatcherAssert.assertThat(mergedContactData(contactInfoFromDetails), CoreMatchers.equalTo(mergedContactData(contactInfoFromEditForm)));
+  }
 
+  private String mergedContactData(ContactData contactData) {
+    return Arrays.asList(contactData.getFirstname(), contactData.getLastname(), contactData.getAllAddresses(),
+            contactData.getHomePhone(), contactData.getMobilePhone(), contactData.getWorkPhone(),
+            contactData.getEmail(), contactData.getEmail2(), contactData.getEmail3())
+            .stream().filter((s -> !s.equals("")))
+            .map(ContactDetailsTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll("\\s+", " ").replaceAll("[-()]", "").replaceAll("^\\s+", "");
+  }
 }
 
