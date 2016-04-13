a9f4d43e897956495a45bb99c1b5ffc0506341e7
ksynnn <fksenium@gmail.com>
2016-04-13 17:55:56 +0300

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/mantis/tests/tests/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.mantis.tests.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.mantis.tests.appmanager.ApplicationManager;
+
+/**
+ * Created by fedotk on 2/29/2016.
+ */
+public class TestBase {
+
+  private final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
+
+}
